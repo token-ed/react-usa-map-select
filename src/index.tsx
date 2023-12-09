66695a42d93b3fdae087ce7071077dbec8766cc5
Ed <emiguel.dev@gmail.com>
2023-12-09 03:03:11 +0100

    Responsive, Fixed West Virginia. Updated version

--- src/index.tsx
+++ src/index.tsx
@@ -103,6 +103,7 @@ export const USAMapSelect = ({
    height="593"
    viewBox="0 0 959 593"
    fill={colorVariants()[USAStateColor]}
+   className="w-full h-full"
   >
    <g>
     {usaState && (
@@ -596,14 +597,14 @@ export const USAMapSelect = ({
      <title>Wisconsin</title>
     </path>
     <path
-     name="West Virgina"
+     name="West Virginia"
      onClick={handleClick}
      onMouseEnter={handleMouseEnter}
      onMouseLeave={handleMouseLeave}
      className={`${onHoverUSAStateColor} ${onHoverUSAStateStrokeColor} hover:cursor-pointer`}
      d="M758.9,254.3 l5.8,-6 2.6,-0.8 1.6,-1.5 1.5,-2.2 1.1,0.3 3.1,-0.2 4.6,-3.6 1.5,-0.5 1.3,1 2.6,1.2 3,3 -0.4,4.3 -5.4,-2.6 -4.8,-1.8 -0.1,5.9 -2.6,5.7 -2.9,2.4 -0.8,2.3 -3,0.5 -1.7,8.1 -2.8,0.2 -1.1,-1 -1.2,-2 -2.2,0.5 -0.5,5.1 -1.8,5.1 -5,11 0.9,1.4 -0.1,2 -2.2,2.5 -1.6,-0.4 -3.1,2.3 -2.8,-0.8 -1.8,4.9 -3.8,1 -2.5,-1.3 -2.5,1.9 -2.3,0.7 -3.2,-0.8 -3.8,-4.5 -3.5,-2.2 -2.5,-2.5 -2.9,-3.7 -0.5,-2.3 -2.8,-1.7 -0.6,-1.3 -0.2,-5.6 0.3,0.1 2.4,-0.2 1.8,-1 v-2.2 l1.7,-1.5 0.1,-5.2 0.9,-3.6 1.1,-0.7 0.4,0.3 1,1.1 1.7,0.5 1.1,-1.3 -1,-3.1 v-1.6 l3.1,-4.6 1.2,-1.3 2,0.5 2.6,-1.8 3.1,-3.4 2.4,-4.1 0.2,-5.6 0.5,-4.8 v-4.9 l-1.1,-3 0.9,-1.3 0.8,-0.7 4.3,19.3 4.3,-0.8 11.2,-1.3z"
     >
-     <title>West Virgina</title>
+     <title>West Virginia</title>
     </path>
     <path
      name="Wyoming"
